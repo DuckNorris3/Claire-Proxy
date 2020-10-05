9aa4b42752c538a2c7e49123ba004b8e4a9e1d02
Claire Melbourne <claire.melbourne@gmail.com>
2020-10-05 17:09:17 -0500

    set up package.json and basic server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello Sweet Friend");
+})
+
+app.listen(PORT, console.log(`Listening at ${PORT}`));
