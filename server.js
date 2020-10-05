688bdc4b572ff92ef4eaa7cefdbb4778fb59eaad
Claire Melbourne <claire.melbourne@gmail.com>
2020-10-05 18:16:14 -0500

    serving empty static file

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello Sweet Friend");
-})
+
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.send("Hello Sweet Friend");
+// });
 
 app.listen(PORT, console.log(`Listening at ${PORT}`));
